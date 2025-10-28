7b8aa548a2ad846fd837f6a299278cb886c1a254
abdallah nabil <abdallahnabil2003@gmail.com>
2025-10-28 15:52:28 +0300

    creating login and sign up pagges

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
- 
-import Home from './pages/Home/Home';
-import Courses from './pages/Courses/Courses';
-import Instructors from './pages/Instructors/Instructors';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+import Home from "./pages/Home/Home";
+import Courses from "./pages/Courses/Courses";
+import Instructors from "./pages/Instructors/Instructors";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/index.jsx";
+import SignUp from "./pages/SignUP/index.jsx";
 function App() {
   return (
     <Router>
@@ -21,6 +22,8 @@ function App() {
             <Route path="/instructors" element={<Instructors />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-Up" element={<SignUp />} />
           </Routes>
         </main>
         <Footer />
